5ee57c9e4d817c2a06e4ce880e93b129a37d9927
somenjeet01 <somenjeet2004@gmail.com>
2024-12-27 17:45:41 +0530

    update with Express Error

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapasync.js");
 
 main()
   .then(() => {
@@ -24,7 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-
 
 app.get("/", (req, res) => {
   res.send("Root working");
@@ -48,11 +48,14 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
 
 //Edit route
 app.get("/listings/:id/edit", async (req, res) => {
@@ -73,6 +76,11 @@ app.delete("/listings/:id", async (req, res) => {
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
+});
+
+//Error handling
+app.use((err, req, res, next) => {
+  res.send("Somenthing went Wrong!");
 });
 
 app.listen(8080, (req, res) => {
