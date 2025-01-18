682033c94f0dc65fdbeff329763477062a562689
somenjeet01 <somenjeet2004@gmail.com>
2025-01-18 06:57:10 +0530

    updated server.js

--- classroom/server.js
+++ classroom/server.js
@@ -12,50 +12,31 @@ app.get("/", (req, res) => {
   res.send("Root working");
 });
 
+//signed cookies
+app.get("/getsignedcookies", (req, res) => {
+  res.cookie("Hello", "India", { signed: true });
+  res.send("signed cookies sent!");
+});
 
+//verify
+app.get("/verify", (req, res) => {
+  // console.log(req.cookies); //for sigened cookies
+  console.log(req.signedCookies); //for unsigned   cookie
+  res.send("verified");
+});
 
+app.get("/getCookies", (req, res) => {
+  res.cookie("Greeet", "Welcome");
+  res.send("sent you some cookies!");
+});
 
+app.get("/greet", (req, res) => {
+  let { name = "annomonous" } = req.cookies;
+  res.send(`Hi,${name}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //signed cookies
-// app.get("/getsignedcookies", (req, res) => {
-//   res.cookie("Hello", "India", { signed: true });
-//   res.send("signed cookies sent!");
-// });
-
-// //verify
-// app.get("/verify", (req, res) => {
-//   // console.log(req.cookies); //for sigened cookies
-//   console.log(req.signedCookies); //for unsigned   cookie
-//   res.send("verified");
-// });
-
-// app.get("/getCookies", (req, res) => {
-//   res.cookie("Greeet", "Welcome");
-//   res.send("sent you some cookies!");
-// });
-
-// app.get("/greet", (req, res) => {
-//   let { name = "annomonous" } = req.cookies;
-//   res.send(`Hi,${name}`);
-// });
-
-// app.use("/", users);
-// app.use("/", posts);
+app.use("/", users);
+app.use("/", posts);
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
