ff0352469a979fb03f7e775eb6c29ccccc25b396
somenjeet01 <somenjeet2004@gmail.com>
2025-01-24 17:24:06 +0530

    connect flash

--- classroom/server.js
+++ classroom/server.js
@@ -2,41 +2,39 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
 
-app.use(cookieParser("secretCode"));
+const sessionOptions = {
+  secret: "mysupersecret",
+  resave: false,
+  saveUninitialized: true,
+};
+app.use(session(sessionOptions));
+app.use(flash());
+app.set("views engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-//root
-app.get("/", (req, res) => {
-  console.dir(req.cookies);
-  res.send("Root working");
+// app.use("/test_count", (req, res) => {
+//   if (req.session.count) {
+//     req.session.count++;
+//   } else {
+//     req.session.count = 1;
+//   }
+//   res.send(`you send a request to ${req.session.count} Times`);
+// });
+
+app.get("/register", (req, res) => {
+  let { name = "Anonyomous" } = req.query;
+  console.log(req.session.name);
+  req.session.name = name;
+  req.flash("success", "user register successfully");
+  res.send(name);
 });
 
-//signed cookies
-app.get("/getsignedcookies", (req, res) => {
-  res.cookie("Hello", "India", { signed: true });
-  res.send("signed cookies sent!");
+app.get("/hello", (req, res) => {
+  res.send(`hello,${req.session.name}`);
 });
-
-//verify
-app.get("/verify", (req, res) => {
-  // console.log(req.cookies); //for sigened cookies
-  console.log(req.signedCookies); //for unsigned   cookie
-  res.send("verified");
-});
-
-app.get("/getCookies", (req, res) => {
-  res.cookie("Greeet", "Welcome");
-  res.send("sent you some cookies!");
-});
-
-app.get("/greet", (req, res) => {
-  let { name = "annomonous" } = req.cookies;
-  res.send(`Hi,${name}`);
-});
-
-app.use("/", users);
-app.use("/", posts);
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
